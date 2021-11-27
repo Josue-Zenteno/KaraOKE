412a44cb9632edef7e3529e8292ea5af964892bd
Josue-Zenteno <josuotp@gmail.com>
2021-11-27 14:37:12 +0100

    Footer.tsx v2.0.0
    Footer Component completed

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,3 +1,36 @@
+import { Button, Grid, Link, Typography } from "@mui/material";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import ProjectData from "../../utils/ProjectData";
+import "./Footer.scss";
+
 export default function Footer() {
-  return <div className='Footer'>Footer</div>;
+  return (
+    <div className='footer'>
+      {/* Footer Left Side */}
+      <div>
+        <Grid container spacing={2} alignItems='center'>
+          <Grid item xs>
+            <GitHubIcon color='primary' fontSize='large' />
+          </Grid>
+          <Grid item xs>
+            <Typography variant='h5'>
+              <Link href={ProjectData.ProjectRepository} target='_blank' rel='noreferrer' underline='none' color='primary'>
+                {ProjectData.ProjectName}
+              </Link>
+            </Typography>
+          </Grid>
+        </Grid>
+      </div>
+
+      {/* Footer Right Side */}
+      <div>
+        <Typography fontSize='medium' color='GrayText'>
+          {`Designed and developed by `}
+          <Link href='/about' underline='none' color='primary'>
+            {ProjectData.TeamName}
+          </Link>
+        </Typography>
+      </div>
+    </div>
+  );
 }
