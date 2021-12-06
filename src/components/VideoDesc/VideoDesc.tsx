211e40255df3103768e1aaa03977d502c09ce2c5
Josue-Zenteno <josuotp@gmail.com>
2021-12-06 01:38:39 +0100

    VideoDesc.tsx v2.0.0
    Video Description component completed

--- src/components/VideoDesc/VideoDesc.tsx
+++ src/components/VideoDesc/VideoDesc.tsx
@@ -1,3 +1,19 @@
-export default function VideoDesc() {
-  return <div className='VideoDesc'>VideoDesc</div>;
+import { Grid, Typography } from "@mui/material";
+import "./VideoDesc.scss";
+
+export default function VideoDesc({ video }) {
+  return (
+    <Grid container direction='column' className='VideoDescription'>
+      <Grid item>
+        <Typography variant='h4' className='Title' textAlign='justify'>
+          {video.title}
+        </Typography>
+      </Grid>
+      <Grid item>
+        <Typography variant='inherit' className='Description'>
+          {video.description}
+        </Typography>
+      </Grid>
+    </Grid>
+  );
 }
