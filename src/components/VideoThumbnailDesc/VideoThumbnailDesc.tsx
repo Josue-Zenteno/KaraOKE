510055a96d7252a6373b7773d84e226aca794ed9
Josue-Zenteno <josuotp@gmail.com>
2021-12-06 01:40:42 +0100

    VideoThumbnailDesc.tsx v2.0.0
    VideoThumbnailDesc component completed

--- src/components/VideoThumbnailDesc/VideoThumbnailDesc.tsx
+++ src/components/VideoThumbnailDesc/VideoThumbnailDesc.tsx
@@ -1,3 +1,17 @@
-export default function VideoThumbnailDesc() {
-  return <div className='VideoThumbnailDesc'>VideoThumbnailDesc</div>;
+import { Grid, Typography } from "@mui/material";
+import "./VideoThumbnailDesc.scss";
+
+export default function VideoThumbnailDesc({ video }) {
+  return (
+    <Grid container className='ThumbnailDescription'>
+      <Grid item xs={12}>
+        <Typography variant='subtitle1' className='Title'>
+          {video.title}
+        </Typography>
+      </Grid>
+      <Grid item xs className='ShortDescription'>
+        <Typography variant='caption'>{video.shortDesc}</Typography>
+      </Grid>
+    </Grid>
+  );
 }
