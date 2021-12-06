e03a0ad73c277aa2350570175c0dbc3121aca93d
Josue-Zenteno <josuotp@gmail.com>
2021-12-06 13:55:42 +0100

    HomePage.tsx v2.0.0
    HomePage component completed

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,3 +1,34 @@
+import { Button, Container, Grid, Link, Typography } from "@mui/material";
+import ProjectData from "../../utils/ProjectData";
+
+import "./HomePage.scss";
+
 export default function HomePage() {
-  return <div className='HomePage'>HomePage</div>;
+  return (
+    <Container maxWidth={false}>
+      <Grid container direction='column' className='HomePage'>
+        {/* Banner */}
+        <Grid item xs={12} direction='column' className='Banner'>
+          <Typography className='BannerTitle'>{ProjectData.ProjectName}</Typography>
+          <Typography className='BannerCaption'>Online Knowledge in English</Typography>
+        </Grid>
+
+        {/* Button */}
+        <Grid item xs={12} className='LearnButton'>
+          <Link href='/interactive-video-player' underline='none'>
+            <Button variant='contained' color='secondary' className='Button'>
+              Let's Learn !!
+            </Button>
+          </Link>
+        </Grid>
+
+        {/* Description */}
+        <Grid item xs={12} className='ProjectDescription'>
+          <Typography align='center' className='Text'>
+            {ProjectData.ProjectDescription}
+          </Typography>
+        </Grid>
+      </Grid>
+    </Container>
+  );
 }
