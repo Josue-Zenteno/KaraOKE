df60e6647777caa62f3c19d11eb5663a85f7764e
Josue-Zenteno <josuotp@gmail.com>
2021-12-06 01:46:24 +0100

    NormVideosPage.tsx v2.0.0
    NormVideosPage component completed

--- src/pages/NormVideosPage/NormVideosPage.tsx
+++ src/pages/NormVideosPage/NormVideosPage.tsx
@@ -1,3 +1,55 @@
+import { useState } from "react";
+
+import { Button, Container, Grid } from "@mui/material";
+import VideoPlayer from "../../components/VideoPlayer/VideoPlayer";
+import VideoDesc from "../../components/VideoDesc/VideoDesc";
+import VideoThumbnail from "../../components/VideoThumbnail/VideoThumbnail";
+import VideoThumbnailDesc from "../../components/VideoThumbnailDesc/VideoThumbnailDesc";
+
+import Videos, { Video } from "../../utils/Videos";
+
+import "./NormVideosPage.scss";
+
 export default function NormVideosPage() {
-  return <div className='NormVideosPage'>NormVideos Page</div>;
+  const [currentVideo, setCurrentVideo] = useState(Videos[0]);
+
+  const handleVideoUpdate = (video: Video): void => {
+    setCurrentVideo(video);
+  };
+
+  return (
+    <Container maxWidth='xl'>
+      <Grid container className='NormalVideoPlayer'>
+        {/* Video player */}
+        <Grid item xs={12} lg={9} className='VideoPlayer'>
+          <Grid container>
+            <Grid item xs={12} className='Player'>
+              <VideoPlayer video={currentVideo} />
+            </Grid>
+            <Grid item xs className='Description'>
+              <VideoDesc video={currentVideo} />
+            </Grid>
+          </Grid>
+        </Grid>
+
+        {/* Thumbnails */}
+        <Grid item xs className='Thumbnails'>
+          {Videos.map((Video) => {
+            return (
+              <Button onClick={() => handleVideoUpdate(Video)} fullWidth={true} className='Thumbnail'>
+                <Grid container>
+                  <Grid item xs={12} md>
+                    <VideoThumbnail video={Video} />
+                  </Grid>
+                  <Grid item xs>
+                    <VideoThumbnailDesc video={Video} />
+                  </Grid>
+                </Grid>
+              </Button>
+            );
+          })}
+        </Grid>
+      </Grid>
+    </Container>
+  );
 }
