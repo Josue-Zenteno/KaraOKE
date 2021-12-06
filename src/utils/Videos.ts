438ae33102bf6cf8c062774ede714469729ddbce
Josue-Zenteno <josuotp@gmail.com>
2021-12-06 01:34:51 +0100

    Videos.ts v1.1.0
    Added short description field
    Added more videos

--- src/utils/Videos.ts
+++ src/utils/Videos.ts
@@ -1,9 +1,10 @@
 import Enemy480 from "../assets/videos/enemy-480.mp4";
 import EnemyThumb from "../assets/thumbnails/thumbnail.png";
 
-interface Video {
+export interface Video {
   title: string;
   description: string;
+  shortDesc: string;
   url: string;
   captions: string;
   thumbnail: string;
@@ -13,6 +14,7 @@ const Videos: Video[] = [
   {
     title: "Titulo del video 1",
     description: "Descripcion del video 1",
+    shortDesc: "Short Description",
     url: Enemy480,
     captions: "",
     thumbnail: EnemyThumb,
@@ -20,6 +22,7 @@ const Videos: Video[] = [
   {
     title: "Titulo del video 2",
     description: "Descripcion del video 2",
+    shortDesc: "Short Description",
     url: Enemy480,
     captions: "",
     thumbnail: EnemyThumb,
@@ -27,6 +30,7 @@ const Videos: Video[] = [
   {
     title: "Titulo del video 3",
     description: "Descripcion del video 3",
+    shortDesc: "Short Description",
     url: Enemy480,
     captions: "",
     thumbnail: EnemyThumb,
@@ -34,6 +38,71 @@ const Videos: Video[] = [
   {
     title: "Titulo del video 4",
     description: "Descripcion del video 4",
+    shortDesc: "Short Description",
+    url: Enemy480,
+    captions: "",
+    thumbnail: EnemyThumb,
+  },
+  {
+    title: "Titulo del video 1",
+    description: "Descripcion del video 1",
+    shortDesc: "Short Description",
+    url: Enemy480,
+    captions: "",
+    thumbnail: EnemyThumb,
+  },
+  {
+    title: "Titulo del video 2",
+    description: "Descripcion del video 2",
+    shortDesc: "Short Description",
+    url: Enemy480,
+    captions: "",
+    thumbnail: EnemyThumb,
+  },
+  {
+    title: "Titulo del video 3",
+    description: "Descripcion del video 3",
+    shortDesc: "Short Description",
+    url: Enemy480,
+    captions: "",
+    thumbnail: EnemyThumb,
+  },
+  {
+    title: "Titulo del video 4",
+    description: "Descripcion del video 4",
+    shortDesc: "Short Description",
+    url: Enemy480,
+    captions: "",
+    thumbnail: EnemyThumb,
+  },
+  {
+    title: "Titulo del video 1",
+    description: "Descripcion del video 1",
+    shortDesc: "Short Description",
+    url: Enemy480,
+    captions: "",
+    thumbnail: EnemyThumb,
+  },
+  {
+    title: "Titulo del video 2",
+    description: "Descripcion del video 2",
+    shortDesc: "Short Description",
+    url: Enemy480,
+    captions: "",
+    thumbnail: EnemyThumb,
+  },
+  {
+    title: "Titulo del video 3",
+    description: "Descripcion del video 3",
+    shortDesc: "Short Description",
+    url: Enemy480,
+    captions: "",
+    thumbnail: EnemyThumb,
+  },
+  {
+    title: "Titulo del video 4",
+    description: "Descripcion del video 4",
+    shortDesc: "Short Description",
     url: Enemy480,
     captions: "",
     thumbnail: EnemyThumb,
