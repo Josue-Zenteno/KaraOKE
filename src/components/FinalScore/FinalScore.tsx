a31832fbb5e66749e88aecb068f869bc794132ad
Josue-Zenteno <josuotp@gmail.com>
2021-12-14 16:24:33 +0100

    FinalScore.tsx v1.0.0
    FinalScore component completed

--- src/components/FinalScore/FinalScore.tsx
+++ src/components/FinalScore/FinalScore.tsx
@@ -1,3 +1,19 @@
-export default function FinalScore() {
-  return <div className='FinalScore'>FinalScore</div>;
+import { Grid, Typography } from "@mui/material";
+import "./FinalScore.scss";
+
+export default function FinalScore({ maxScore, finalScore }) {
+  return (
+    <Grid container direction='column' className='FinalScore'>
+      <Grid item xs={12}>
+        <Typography className='Title'>Final Score</Typography>
+      </Grid>
+      <Grid item xs>
+        <Typography className='Score'>
+          {finalScore}
+          {" / "}
+          {maxScore}
+        </Typography>
+      </Grid>
+    </Grid>
+  );
 }
