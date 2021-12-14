3c67ea1fa91413a586d70a69ed3751a390101e39
Josue-Zenteno <josuotp@gmail.com>
2021-12-14 16:26:05 +0100

    InterVideosPage.tsx v2.1.0
    InterVideosPage component completed.

--- src/pages/InterVideosPage/InterVideosPage.tsx
+++ src/pages/InterVideosPage/InterVideosPage.tsx
@@ -15,12 +15,16 @@ import "./InterVideosPage.scss";
 
 let intervalCaptions: number;
 let currentLyrics: Sentence[];
+let currentAnswers: string[];
+let currentUserAnswers: string[];
 let videoPlayer = createRef<ReactPlayer>();
 
 export default function InterVideosPage() {
   const [currentVideo, setCurrentVideo] = useState(Videos[0]);
+  const [currentQuestions, setCurrentQuestions] = useState<any>();
   const [currentCaption, setCurrentCaption] = useState<string>("*Captions*");
   const [finalScoreComponent, setFinalScoreComponent] = useState(<></>);
+  const [videoPlayPause, setVideoPlayPause] = useState<boolean>(false);
 
   // Handlers
   const handleChangeVideo = (video: Video): void => {
@@ -34,18 +38,28 @@ export default function InterVideosPage() {
 
   const handleStartVideo = (): void => {
     setupLyrics();
+    setupAnswers();
+    setupUserAnswers();
   };
 
   const handlePlayVideo = (): void => {
+    setVideoPlayPause(true);
     startCaptions();
   };
 
   const handlePauseVideo = (): void => {
+    setVideoPlayPause(false);
     stopCaptions();
   };
 
   const handleEndVideo = (): void => {
     showFinalScore();
+  };
+
+  const handleOptionSelected = (option: string): void => {
+    setCurrentQuestions(undefined);
+    currentUserAnswers.push(option);
+    setVideoPlayPause(true);
   };
 
   // Methods
@@ -57,14 +71,44 @@ export default function InterVideosPage() {
     currentLyrics = currentVideo.captions;
   };
 
+  const setupAnswers = (): void => {
+    currentAnswers = currentVideo.answers;
+  };
+
+  const setupUserAnswers = () => {
+    currentUserAnswers = [];
+  };
+
   const startCaptions = (): void => {
     intervalCaptions = setInterval(() => {
-      let currentTime = videoPlayer?.current?.getCurrentTime();
-      let currentSentence = findSentence(currentTime)?.text;
-      if (currentSentence) {
-        setCurrentCaption(currentSentence);
+      let questions = updateCaptions();
+      if (questions) {
+        setupQuestions(questions);
+        pauseVideoQuestion();
       }
-    }, 1000);
+    }, 900);
+  };
+
+  const pauseVideoQuestion = () => {
+    setVideoPlayPause(false);
+  };
+
+  const updateCaptions = (): string[] | undefined => {
+    let currentTime = videoPlayer?.current?.getCurrentTime();
+    let currentSentence = findSentence(currentTime);
+    if (currentSentence) {
+      setCurrentCaption(currentSentence.text);
+
+      if (currentSentence.question) {
+        return currentSentence.posibilities;
+      }
+    }
+
+    return undefined;
+  };
+
+  const setupQuestions = (questions: string[]): void => {
+    setCurrentQuestions(questions);
   };
 
   const stopCaptions = (): void => {
@@ -76,7 +120,32 @@ export default function InterVideosPage() {
   };
 
   const showFinalScore = (): void => {
-    setFinalScoreComponent(<FinalScore />);
+    let finalScore: number = calulateFinalScore();
+    let maxScore: number = currentAnswers.length;
+    setFinalScoreComponent(<FinalScore maxScore={maxScore} finalScore={finalScore} />);
+  };
+
+  const calulateFinalScore = (): number => {
+    let parsedCurrentUserAnswers = parseUserAnswers();
+    let finalAnswers;
+
+    if (parsedCurrentUserAnswers.length > currentAnswers.length) {
+      finalAnswers = parsedCurrentUserAnswers.filter((answer) => !currentAnswers.includes(answer));
+    } else {
+      finalAnswers = currentAnswers.filter((answer) => !parsedCurrentUserAnswers.includes(answer));
+    }
+
+    console.log(finalAnswers);
+    if (finalAnswers.length > 0) {
+      return currentAnswers.length - finalAnswers.length;
+    }
+    return currentAnswers.length;
+  };
+
+  const parseUserAnswers = (): string[] => {
+    let currenUserAnswersSet = new Set(currentUserAnswers);
+    let parsedUserAnswers = Array.from(currenUserAnswersSet);
+    return parsedUserAnswers;
   };
 
   const hideFinalScore = (): void => {
@@ -85,8 +154,8 @@ export default function InterVideosPage() {
 
   //Aux Methods
   const findSentence = (currentTime: any): any => {
-    let aux = currentLyrics.find((sentence) => sentence.time == Math.round(currentTime));
-    return aux;
+    let auxSentence: Sentence | undefined = currentLyrics.find((sentence) => sentence.time == Math.round(currentTime));
+    return auxSentence;
   };
 
   return (
@@ -106,6 +175,7 @@ export default function InterVideosPage() {
                 onEnded={() => handleEndVideo()}
                 url={currentVideo.url}
                 controls={true}
+                playing={videoPlayPause}
                 width='100%'
                 height='100%'
               />
@@ -121,7 +191,13 @@ export default function InterVideosPage() {
               <Grid container direction='row-reverse' className='DescriptionSection'>
                 {/* Option buttons */}
                 <Grid item xs={12} md className='OptionButtons'>
-                  <OptionButton />
+                  {currentQuestions?.map((option: string) => {
+                    return (
+                      <Button key={option} variant='contained' onClick={() => handleOptionSelected(option)} className='Button'>
+                        {option}
+                      </Button>
+                    );
+                  })}
                 </Grid>
 
                 {/* Description */}
@@ -137,13 +213,15 @@ export default function InterVideosPage() {
         <Grid item xs className='Thumbnails'>
           <Grid container direction='column'>
             {/* Final score */}
-            <Grid item>{finalScoreComponent}</Grid>
+            <Grid item className='FinalScoreSection'>
+              {finalScoreComponent}
+            </Grid>
 
             {/* Thumbnails */}
             <Grid item>
               {Videos.map((Video) => {
                 return (
-                  <Button key={Video.title} onClick={() => handleChangeVideo(Video)} fullWidth={true} className='Thumbnail'>
+                  <Button key={Video.title} onClick={() => handleChangeVideo(Video)} fullWidth={true} className='ThumbnailContainer'>
                     <Grid container>
                       <Grid item xs={12} md sx={{ backgroundColor: "rgba(0, 0, 0, 0.438)" }}>
                         <VideoThumbnail video={Video} />
